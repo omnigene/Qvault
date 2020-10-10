19e4ea1a47a4e27869f9deeaf8e8cb58c22e0a9c
omnigene <omnigene@protonmail.com>
2020-10-10 17:10:58 +0800

    Complete iframe auto resize & mobile and email switch function

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,14 +1,32 @@
 $(function () {
     $('#login-btn').click(function () {
-        var mainframe=$("#mainframe");
-        mainframe.attr("src","/login");
-        mainframe[0].onload=function () {
-            var framebody=mainframe[0].contentDocument.body;
-            var ro = new ResizeObserver( entries => {
-                reheight=entries[0].contentRect.height+65;
-                $(this).attr("height",reheight);
+        var mainFrame=$("#mainframe");
+        mainFrame.attr("src","/login");
+        // mainFrame加载完毕后执行
+        mainFrame.on('load',function () {
+            var toRegister=mainFrame.contents().find("#to-register");
+            toRegister.click(function () {
+                mainFrame.attr("height","568px");
             });
-            ro.observe(framebody);
-        };
+            var frameBody=mainFrame.contents().find("body")[0];
+            var ro = new ResizeObserver( entries => {
+                resizeHeight=entries[0].contentRect.height+90;
+                $(this).attr("height",resizeHeight);
+            });
+            ro.observe(frameBody);
+        })
+
+        // mainFrame[0].onload=function () {
+        //     var toRegister=mainFrame[0].contentDocument.getElementById("to-register");
+        //     console.log(toRegister);
+        //     var i=$("#to-register")[0];
+        //     console.log(i);
+        //     // var frameBody=mainFrame[0].contentDocument.body;
+        //     // var ro = new ResizeObserver( entries => {
+        //     //     resizeHeight=entries[0].contentRect.height+65;
+        //     //     $(this).attr("height",resizeHeight);
+        //     // });
+        //     // ro.observe(frameBody);
+        // };
     });
 });
--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,3 +1,10 @@
+// 阻止按回车键提交表单
+$(document).keydown(function (event) {
+    if (event.which === 13) {
+        return false;
+    }
+});
+
 $(document).ready(function () {
     var errors={
         'mobile':['您还未填写手机号码。','请输入11位有效手机号码，不支持虚拟运营商号段。','该手机号已注册！'],
@@ -142,7 +149,7 @@ $(document).ready(function () {
             captcha1.show();
         }
     });
-    var activePanel=($('.tab-pane.active'));
+    var activePanel=$('.tab-pane.active');
     $("#next").click(function () {
         activePanel.find("input").focus();
         // checkRegistered(username);
@@ -174,13 +181,26 @@ $(document).ready(function () {
             $(".email-tag").css("color","lightgray");
             $(".input-wrap.email").css('display','none');
             $(".input-wrap.mobile").css('display','');
+            if (mobile.hasClass("check-icon")){
+                console.log("选择手机：状态"+mobile.hasClass("check-icon"));
+                $("#code,#send").removeAttr("disabled");
+            }else{
+                console.log("选择手机：状态"+mobile.hasClass("check-icon"));
+                $("#code,#send").attr("disabled","true");
+            }
         }
         else{
             $(".mobile-tag").css("color","lightgray");
             $(".email-tag").css("color","black");
             $(".input-wrap.mobile").css('display','none');
             $(".input-wrap.email").css('display','block');
+            if (email.hasClass("check-icon")){
+                console.log("选择邮件：状态"+email.hasClass("check-icon"));
+                $("#code,#send").removeAttr("disabled");
+            }else{
+                console.log("选择邮件：状态"+email.hasClass("check-icon"));
+                $("#code,#send").attr("disabled","true");
+            }
         }
     });
-
 });
